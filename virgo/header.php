732f02e60c6cf2f05034066b48913cac45a8b248
Nghi Le <vinhnghi223@gmail.com>
2014-09-14 12:52:47 +0300

    Update header.php

--- virgo/header.php
+++ virgo/header.php
@@ -8,23 +8,9 @@
 	<title></title>
 	
 	<link rel="EditURI" type="application/rsd+xml" title="RSD" href="xmlrpc0db0.php?rsd" />
-<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://dice.se/wp-includes/wlwmanifest.xml" /> 
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" /> 
 <meta name="generator" content="WordPress 3.4.1" />
  
-	<script type="text/javascript">
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-29039634-1']);
-		_gaq.push(['_setDomainName', 'dice.se']);
-		_gaq.push(['_trackPageview']);
-
-		(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
-	</script>
-
-
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	
 	<?php if (is_search()) { ?>
